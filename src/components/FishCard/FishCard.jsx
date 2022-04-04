dee47542dc10cf852e384ecc9d76ad18e738fef7
djrich87 <djrich87@gmail.com>
2022-04-03 22:47:20 -0600

    add Fish photos

--- src/components/FishCard/FishCard.jsx
+++ src/components/FishCard/FishCard.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom'
 
-function FishCard({fish}) {
+
+function FishCard({fish, randFishImgId}) {
   return(
     <div className="card">
+    <img 
+      src={`https://picsum.photos/id/${randFishImgId}/640/480`} 
+      alt="A happy fish"
+      className="card-img-top" 
+      />
+
       <div className="card-body">
         <h2 className="card-text">{fish.name}</h2>
         <p className="card-text">A {fish.age}-year-old {fish.breed}</p>
--- src/pages/FishList/FishList.jsx
+++ src/pages/FishList/FishList.jsx
@@ -1,6 +1,9 @@
 import styles from './FishList.module.css'
 import FishCard from '../../components/FishCard/FishCard'
 
+const fishIds = [1025,1012,1062,1084,169,200,219,237,244,275,40,433,577,582,
+	593,611,659,718,783,790,824,837,881,937,943]
+
 function FishList(props) {
   
   return (
@@ -8,7 +11,9 @@ function FishList(props) {
       <h1>Fish List</h1>
       <div className={styles.container}>
         {props.fishes.map(fish => (
-           <FishCard key={fish._id} fish={fish}/>
+           <FishCard key={fish._id} fish={fish} 
+           randFishImgId={fishIds[Math.floor(Math.random()*(fishIds.length))]}
+           />
         ))}
       </div>
     </>
